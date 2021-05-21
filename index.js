254e8f4730a9ccadb7d86e4526b86dbf1d8fe06c
unknown <rubayzaf2007@gmail.com>
2021-05-21 13:08:36 +0600

    some tenor work done!

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let fetch = require("node-fetch");
 module.exports = {
-    gif: async function(keyWord, tenorAPI, limit) {
-        const url = `https://api.tenor.com/v1/search?q=${keyWord}&key=${tenorAPI}&limit=${limit}`;
-        const response = await fetch(url);
+    getGIFBySeachTerm: async function(keyWord, tenorAPI, limit) {
+        let SearchUrl = `https://g.tenor.com/v1/search?q=${keyWord}&key=${tenorAPI}&limit=${limit}`;
+        const response = await fetch(SearchUrl);
         const result = await response.json();
         const index = Math.floor(Math.random() * result.results.length);
-        console.log(result.results[index].url)
+        console.log(`Your searched gif: ${result.results[index].url}`)
+    },
+    getTrendingGIFs: async function(tenorAPI, limit) {
+        let trendingUrl = `https://g.tenor.com/v1/trending?key=${tenorAPI}&limit=${limit}`;
+        const trendingResponse = await fetch(trendingUrl);
+        const trendingResult = await trendingResponse.json();
+        const trendingIndex = Math.floor(Math.random() * trendingResult.results.length);
+        console.log(`One of the trending gifs: ${trendingResult.results[trendingIndex].url}`);
+    },
+    getGIFById: async function(gifId, tenorAPI) {
+        let idGifUrl = `https://g.tenor.com/v1/gifs?ids=${gifId}&key=${tenorAPI}`;
+        const idGifResponse = await fetch(idGifUrl);
+        const idGifResult = await idGifResponse.json();
+        const idGifIndex = Math.floor(Math.random() * idGifResult.results.length);
+        console.log(`Here's your gif with the id: ${idGifResult.results[idGifIndex].url}`);
     }
 }
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const pckg = require("./index");
 const config = require("./config.json");
-pckg.gif("hug", config.tenorAPI, "4");
+pckg.getGIFBySeachTerm("hug", config.tenorAPI, "4");
+pckg.getTrendingGIFs(config.tenorAPI, "4");
+pckg.getGIFById(8776030, config.tenorAPI);
